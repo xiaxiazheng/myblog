28da1b8e386c2ba554f65bbe17c3a5aedccef059
xiaxiazheng <13075218875@163.com>
2018-07-23 00:34:06 +0800

    搭了个框架
    用vue-cli3.0的rc版搭了个大概，less，router，element什么的也引入了

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="less" scoped>
+  .hello {
+    h1 {
+      color: red;
+    }
+  }
+</style>
